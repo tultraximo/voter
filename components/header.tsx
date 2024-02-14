a12d314f8a62927f7e72d97ee33723edb9b266ae
tultraximo <jodeelmoody@gmail.com>
2024-02-14 14:22:43 -0800

    dark mode disabled1

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Header() {
     return (
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg ">
-      <div className="fixed left-0 top-0 flex w-full justify-evenly border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+      <div className="fixed left-0 top-0 flex w-full justify-evenly border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl">
       <Link href="/about-us" className="header-link">About Us</Link>
       <Link href="/product" className="header-link">Product</Link>
       <Link href="/faq" className="header-link">FAQ</Link>
