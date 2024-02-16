cd7ccb696d5c6bcb5a2db696c86425361a5dbc20
tultraximo <jodeelmoody@gmail.com>
2024-02-15 20:55:21 -0800

    fonts extra

--- components/card.tsx
+++ components/card.tsx
@@ -7,7 +7,7 @@ export default function Card() {
   const description = "NUMBER OF US COLLEGE-AGE VOTERS"
   return (
     <div
-      className="relative w-128 overflow-hidden rounded border-gray-100 border-solid
+      className="relative w-128  overflow-hidden rounded border-gray-100 border-solid
        bg-gray-200 shadow-lg
         m-4"
     >
@@ -15,7 +15,7 @@ export default function Card() {
         <div className="m-4 mb-5 bg-gradient-to-br from-gray-900 via-gray-900 to-gray-900 bg-clip-text text-transparent text-6xl sm:text-8xl font-bold  [text-wrap:balance]">
           {title}
         </div>
-        <div className="rounded shadow-lg text-3xl sm:text-4xl pb-5 mx-12 px-2 font-display
+        <div className="rounded shadow-lg font-noto text-3xl sm:text-4xl pb-5 mx-12 px-2 font-display
           font-medium text-gray-800 bg-gray-200 [text-wrap:balance]">
             {description}</div>
       </div>
--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg ">
+      <div className="z-10 font-noto max-w-5xl w-full items-center justify-between font-mono text-lg ">
       <div className="fixed left-0 top-0 flex w-full justify-evenly border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl">
-      <Link href="/about-us" className="header-link">About Us</Link>
-      <Link href="/product" className="header-link">Product</Link>
+      <Link href="/about-us" className="header-link">ABOUT US</Link>
+      <Link href="/product" className="header-link">PRODUCT</Link>
       <Link href="/faq" className="header-link">FAQ</Link>
     </div>
     </div>
